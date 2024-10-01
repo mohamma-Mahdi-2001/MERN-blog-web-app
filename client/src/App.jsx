5ff3f4569ac8004cec477076c927f2070d6f89a4
Mahdi <mahdigulistani0@gmail.com>
2024-10-01 15:24:59 +0430

    footer component created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import About from "./pages/About"
 import Home from "./pages/Home"
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
+import FooterCom from "./components/Footer";
 
 
 export default function App() {
@@ -22,6 +23,7 @@ export default function App() {
         <Route path="/projects" element={<Projects/>}/>
      
       </Routes> 
+      <FooterCom/>
      
     </BrowserRouter>
   );
