2b43c10cbd1ea8b37c69530e5649c9f96e907d67
Mahdi <mahdigulistani0@gmail.com>
2024-10-17 14:05:17 +0430

    compelete create a post UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,9 @@ import Home from "./pages/Home"
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
 import FooterCom from "./components/Footer";
-import PrivateRoute from "./components/PrivateRoute";
+import PrivateRoute from "./components/OnlyAdminPrivateRoute";
+import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
+import CreatePost from "./pages/CreatePost";
 
 
 export default function App() {
@@ -19,8 +21,9 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route element={<PrivateRoute />}>
+        <Route element={<OnlyAdminPrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/create-post" element={<CreatePost />} />
         </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -242,9 +242,25 @@ const handleSignout = async () => {
           placeholder="password"
           onChange={handleChange}
         />
-        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
-          Uodate
+        <Button
+          type="submit"
+          gradientDuoTone="purpleToBlue"
+          outline
+          disabled={loading || imageFileUploading}
+        >
+          {loading ? "Loading..." : "Update"}
         </Button>
+        {currentUser.isAdmin && (
+          <Link to={"/create-post"}>
+            <Button
+              type="button"
+              gradientDuoTone="purpleToPink"
+              className="w-full"
+            >
+              Create a post
+            </Button>
+          </Link>
+        )}
       </form>
       <div className="text-red-500 flex justify-between mt-5">
         <span onClick={() => setShowModal(true)} className="cursor-pointer">
