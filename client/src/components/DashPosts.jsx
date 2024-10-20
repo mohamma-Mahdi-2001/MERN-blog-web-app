6d59d12bb97c29ef33d743f82824ac2d51f04efb
Mahdi <mahdigulistani0@gmail.com>
2024-10-20 13:53:52 +0430

    add show more functionality to posts result in dsshboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 export default function DashPosts() {
 const { currentUser } = useSelector((state) => state.user);
 const [userPosts, setUserPosts] = useState([]);
+ const [showMore, setShowMore] = useState(true);
 useEffect(() => {
   const fetchPosts = async () => {
     try {
@@ -25,6 +26,24 @@ useEffect(() => {
     fetchPosts();
   }
 }, [currentUser._id]);
+
+ const handleShowMore = async () => {
+   const startIndex = userPosts.length;
+   try {
+     const res = await fetch(
+       `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+     );
+     const data = await res.json();
+     if (res.ok) {
+       setUserPosts((prev) => [...prev, ...data.posts]);
+       if (data.posts.length < 9) {
+         setShowMore(false);
+       }
+     }
+   } catch (error) {
+     console.log(error.message);
+   }
+ };
 
 
 
@@ -90,6 +109,14 @@ useEffect(() => {
               </Table.Body>
             ))}
           </Table>
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className="w-full text-teal-500 self-center text-sm py-7"
+            >
+              Show more
+            </button>
+          )}
         </>
       ) : (
         <p>you have np post</p>
