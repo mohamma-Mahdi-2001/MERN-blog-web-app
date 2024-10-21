50616d89ecb02cdc1f102a33c78647add401e7d6
Mahdi <mahdigulistani0@gmail.com>
2024-10-21 11:18:00 +0430

    add show uses in admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import { useLocation } from "react-router-dom";
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile";
 import DashPosts from '../components/DashPosts';
+import DashUsers from '../components/DashUsers';
 
 
 export default function Dashboard() {
@@ -25,7 +26,10 @@ export default function Dashboard() {
       </div>
       {/* profile... */}
       {tab === "profile" && <DashProfile />}
+      {/*posts*/}
       {tab === "posts" && <DashPosts />}
+      {/* users */}
+      {tab === "users" && <DashUsers />}
     </div>
   );
 }
